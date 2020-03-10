383f33de06da07026334784f325f3cbd6d204bf1
한대호 <supreme2705@daou.co.kr>
2020-03-10 16:21:49 +0900

    feat: gitclient 설치 bat추가

---
+++ gitclient.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo author : Daeho Han(supreme2705@gmail.com)
+echo title : source tree install
+echo desc : install chocolatey and git client
+
+pause
+
+choco -v
+if not "%ERRORLEVEL%" == "0" goto CHOCO_INSTALL
+goto CHOCO_EXIST
+
+:CHOCO_INSTALL
+echo. install chocolatey(https://chocolatey.org)
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+goto GIT_CLIENT_INSTALL
+goto QUIT
+
+
+:GIT_CLIENT_INSTALL
+choco install git
+goto QUIT
+
+:CHOCO_EXIST
+echo "choco is already installed !"
+goto GIT_CLIENT_INSTALL
+
+
+:QUIT
